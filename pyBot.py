da9be571a96e811414fd66aed2d0144716a34211
Jigsy <34282672+Jigsy1@users.noreply.github.com>
2023-01-23 05:41:19 +0000

    Add files via upload
    Commands parsed as dictionary entries. Also should gracefully exit now if the connection dies.

--- pyBot.py
+++ pyBot.py
@@ -5,17 +5,21 @@
 import socket
 
 
-# define(s):
+# IRC command(s):
 
-BOT_CHAN = "#localhost"
-BOT_NICK = "pyBot"
-BOT_PORT = 6667
-BOT_REALNAME = "%s" % (BOT_NICK)
-BOT_SERVER = "localhost"
-NEW_LINES = "\r\n"
+def parse_irc_command(command, data):
+    IRC_COMMANDS[command](data)
 
-pyBot = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-pyBot.connect((BOT_SERVER, BOT_PORT))
+def parse_irc_376(data):
+    # `-> <server> 376 <nick> :End of /MOTD command.
+
+    sendRaw("JOIN {}{}".format(BOT_CHAN, NEW_LINES))
+
+def parse_irc_ping(data):
+    # `-> PING [:]<arg>
+
+    data = data.split(" ")
+    sendRaw("PONG {}{}".format(data[1], NEW_LINES))
 
 
 # Function(s):
@@ -23,6 +27,23 @@ pyBot.connect((BOT_SERVER, BOT_PORT))
 def sendRaw(input):
     pyBot.send(input.encode("UTF-8"))
     print(input.strip(NEW_LINES))
+
+
+# define(s):
+
+BOT_CHAN = "#localhost"
+BOT_NICK = "pyBot"
+BOT_PORT = 6667
+BOT_REALNAME = "%s" % (BOT_NICK)
+BOT_SERVER = "localhost"
+IRC_COMMANDS = {
+    '376': parse_irc_376,
+    'PING': parse_irc_ping
+}
+NEW_LINES = "\r\n"
+
+pyBot = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pyBot.connect((BOT_SERVER, BOT_PORT))
 
 
 # Core:
@@ -33,18 +54,22 @@ def main():
 
     while 1:
             data = pyBot.recv(4096).decode("UTF-8")
+            if not data:
+                break
             print(data)
             # `-> No line stripping in this as I noticed on one server that they have an \r in the MOTD line(s).
-            data = data.split(NEW_LINES)
-            for line in data:
+            newData = data.split(NEW_LINES)
+            if len(data.strip(NEW_LINES)) == 0:
+                return
+            for line in newData:
                 newLine = line.split(" ")
                 if len(newLine) > 1:
-                    if newLine[1] == "376":
-                        # `-> <server> 376 <nick> :End of /MOTD command.
-                        sendRaw("JOIN {}{}".format(BOT_CHAN, NEW_LINES))
-                if newLine[0] == "PING":
-                    # `-> PING [:]<arg>
-                    sendRaw("PONG {}{}".format(newLine[1], NEW_LINES))
+                    if newLine[1] in IRC_COMMANDS:
+                        parse_irc_command(newLine[1], line)
+                if newLine[0] in IRC_COMMANDS:
+                    parse_irc_command(newLine[0], line)
+    pyBot.close()
+    print("Socket closed.\n")
 
 if __name__ == "__main__":
     main()
